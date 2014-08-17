98bf9407e58400a09af61e8917e5a43d169559c3
Chris Caron <lead2gold@gmail.com>
2014-08-17 16:05:34 -0400

    adjusted copywrite information

--- Subliminal.py
+++ Subliminal.py
@@ -3,7 +3,7 @@
 #
 # Subliminal post-processing script for NZBGet
 #
-# Copyright (C) 2013-2014 Andrey Prygunkov <hugbug@users.sourceforge.net>
+# Copyright (C) 2014 Chris Caron <lead2gold@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by
