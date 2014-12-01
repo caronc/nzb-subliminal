b8c017f090b1da96ed8dfa850dab9b841a4886da
Chris Caron <lead2gold@gmail.com>
2014-12-01 14:42:40 -0500

    set version to 0.9.2

--- Subliminal.py
+++ Subliminal.py
@@ -33,9 +33,9 @@
 #
 # Info about this Subliminal NZB Script:
 # Author: Chris Caron (lead2gold@gmail.com).
-# Date: Sun, Nov 11th, 2014.
+# Date: Sun, Dec  1st, 2014.
 # License: GPLv3 (http://www.gnu.org/licenses/gpl.html).
-# Script Version: 0.9.1.
+# Script Version: 0.9.2.
 #
 # NOTE: This script requires Python to be installed on your system.
 #
