11818fc6082306c742fefcb9842f10e085d64f2f
Chris Caron <lead2gold@gmail.com>
2017-09-17 19:04:35 -0400

    better python >= 2.6 support

--- Subliminal/nzbget/PostProcessScript.py
+++ Subliminal/nzbget/PostProcessScript.py
@@ -161,11 +161,11 @@ from os.path import abspath
 from socket import error as SocketError
 
 # Relative Includes
-from ScriptBase import ScriptBase
-from ScriptBase import Health
-from ScriptBase import SCRIPT_MODE
-from ScriptBase import NZBGET_BOOL_FALSE
-from Utils import os_path_split as split
+from .ScriptBase import ScriptBase
+from .ScriptBase import Health
+from .ScriptBase import SCRIPT_MODE
+from .ScriptBase import NZBGET_BOOL_FALSE
+from .Utils import os_path_split as split
 
 # Obfuscated Expression
 OBFUSCATED_PATH_RE = re.compile(
@@ -648,7 +648,7 @@ class PostProcessScript(ScriptBase):
             group = [ x for x in self.api.listgroups(0) \
                      if x['NZBID'] == nzbid ][0]
 
-        except SocketError, e:
+        except SocketError as e:
             self.logger.warning('RCP Connection Failure (%d): %s' % (
                 e[0], e[1]))
             return None
